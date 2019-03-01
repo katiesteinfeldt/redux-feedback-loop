e2865a7ab8741a14e0d4d6cbfe46c2e54f4c6797
Katie Mangan <katie.mangan1@gmail.com>
2019-03-01 16:56:56 -0600

    send feelings information to redux state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,9 +6,26 @@ import './App.css';
 
 class App extends Component {
 
-  componentDidMount() { 
+    state = {
+      feelings: '',
+    }
+
+  componentDidMount() {
     //this.getPizzas();
   }
+
+  handleButtonClick = () => {
+    console.log('button was clicked');
+    this.props.dispatch({type: 'SUBMIT_FEELINGS', payload: this.state.feelings})
+  }
+
+  handleChangeFor = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      feelings: event.target.value,
+    })
+  }
+
 
   render() {
     return (
@@ -17,7 +34,17 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
-        <br/>
+        <div>
+          <h1>How are you feeling today?</h1>
+          <br />
+          <label for="feeling">Feeling?</label>
+          <br />
+          <input onChange={this.handleChangeFor} id="feeling" type="number"></input>
+          <br />
+          <div className="nextButton">
+            <button onClick={this.handleButtonClick}>Next</button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 //import registerServiceWorker from './registerServiceWorker';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 // Redux
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
+const feelingsReducer = (state = [], action) => {
+    console.log(action.payload);
+    if (action.type === 'SET_FEELINGS') {
+        return action.payload;
+    }
+    return state;
+}
 
 
 const storeInstance = createStore(
     combineReducers({
-      
+      feelingsReducer
     }),
-    // applyMiddleware(logger),
+    applyMiddleware(logger),
 );
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
