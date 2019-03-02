64793dcccd01a2a8e1aa8395a131276076f3e398
Katie Mangan <katie.mangan1@gmail.com>
2019-03-02 10:08:16 -0600

    leave new feedback button

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 
 class Thanks extends Component {
 
+    newFeedback = () => {
+        console.log('new feedback was clicked');
+    }
+
     render() {
         return (
             <div>
                 <h1>Thank you!</h1>
+                <br />
+                <button onClick={this.newFeedback}>Leave New Feedback</button>
             </div>
         );
     }
